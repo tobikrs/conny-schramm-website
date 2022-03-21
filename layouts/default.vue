f671fda93c8e798894538b5a5baa942f0a0a036a
Tobias <tobias_krause@akranet.de>
2022-03-21 16:16:25 +0100

    mobile menu wip

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,16 @@
 <template lang="pug">
-.website-wrapper.min-h-screen.flex.flex-col
-  header.bg-white.z-10.shadow-lg.sticky.top-0
-    .max-w-screen-xl.min-h-21.mx-auto.p-3.flex.items-center.justify-between.font-serif
-      nuxt-link(to='/')
+.website-wrapper.min-h-screen.flex.flex-col(:class="mobileMenu ? 'h-screen overflow-hidden' : undefined")
+  header.bg-white.z-10.shadow-lg.top-0(:class="mobileMenu ? 'fixed inset-0' : 'sticky'")
+    .max-w-screen-xl.min-h-21.mx-auto.p-3.flex.items-center.font-serif.flex-wrap.h-full
+      nuxt-link(to='/').mr-auto
         .text-2xl.font-black.text-red-800.whitespace-nowrap.leading-none Conny Schramm
         .text-sm.uppercase.tracking-wider.opacity-75 Autorin
-      nav(role='navigation')
-        ul.flex.justify-between.h-full.items-center.gap-2.font-sans.font-semibold
+      button.w-12.h-12.p-3.-mr-1.rounded-full(@click="mobileMenu = !mobileMenu")
+        MenuIcon.w-full.h-full
+      nav(role='navigation').w-full.h-full
+        ul.flex.justify-between.items-center.gap-2.font-sans.font-semibold.text-center(:class="mobileMenu ? 'flex-col' : undefined")
           li(v-for='(mItem, i) in menuItems', :key='`${mItem.link}-${i}`', :class='mItem.class || ""')
             nuxt-link(:to='mItem.link', :title='mItem.label') {{ mItem.label }}
-          li.only-mobile.w-12.h-12
-            nuxt-link.absolute.inset-0.p-2.rounded-full(to='menu', role='button')
-              MenuIcon.w-full.h-full
   nuxt
   footer.bg-gray-800.text-white.h-32.mt-auto
     pre TODO: Footer mit Inhalt füllen
@@ -29,6 +28,8 @@ export default defineComponent({
   },
   setup() {
     const menuItems = ref([])
+
+    const mobileMenu = ref(true)
 
     menuItems.value = [
       {
@@ -56,25 +57,23 @@ export default defineComponent({
 
     return {
       menuItems,
+      mobileMenu
     }
   },
 })
 </script>
 
 <style scoped>
-nav li:not(.only-mobile) {
-  @apply hidden md:inline-block;
-}
-
-nav li.only-mobile {
+.mobile-menu-btn {
   @apply relative inline-block md:hidden;
 }
 
-nav li > a {
-  @apply hover:bg-gray-100 text-red-800 whitespace-nowrap;
+nav li {
+  @apply block md:inline-block;
 }
-nav li:not(.only-mobile) > a {
-  @apply px-4 py-2 rounded-lg;
+
+nav li > a {
+  @apply hover:bg-gray-100 text-red-800 whitespace-nowrap px-4 py-2 rounded-lg;
 }
 
 nav li.call-to-action > a {
